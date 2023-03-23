a282255df6277ff415ccfd7f7f0530be49948cf5
Daniel Middleton <daniel.middleton05@gmail.com>
2023-03-23 17:54:10 +0900

    CampsitesDirectoryPage and CampsiteDetail

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import CampsitesList from './features/campsites/CampsitesList.js';
-import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+import './App.css';
 
 function App() {
     return (
         <div className='App'>
-            <Header/>
-            <CampsitesList/>
-            <Footer/>
+            <Header />
+            <CampsitesDirectoryPage />
+            <Footer />
         </div>
     );
 }
